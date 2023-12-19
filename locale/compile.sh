ae89a660132d3be1452cbe1e9d1657f09bc87ed5
nicole <145790372+equqe@users.noreply.github.com>
2023-12-20 02:04:37 +0300

    Add files via upload

---
+++ locale/compile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "${PWD##*/}" = "locale" ];
+then
+	function compile {
+		# shellcheck disable=SC2164
+		cd './'"$1"'/LC_MESSAGES/'
+		msgfmt bbot.po -o bbot.mo
+		cd ../../
+	};
+else
+	exit 1;
+fi
+
+
+compile ru_RU
+compile en_GB
